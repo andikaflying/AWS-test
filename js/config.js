5921085b1c47027e4198f2c6b5339f511f73d156
Andika Kurniawan <44762285+andikaflying@users.noreply.github.com>
2026-07-19 18:42:26 +0700

    refactor: adding

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_p8fjVkdwj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4of94n6sk3964mnr11hs6gkb2h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
