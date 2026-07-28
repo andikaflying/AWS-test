f3775bc7fd0b89ae26a1ca66ada9cd0789683c70
Andika Kurniawan <44762285+andikaflying@users.noreply.github.com>
2026-07-28 13:01:11 +0700

    refactor: update config for Cognito user pool

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-1_rSspGA6Iz', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5v6gipfsh3n224s1fvmphpdee2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-1_p8fjVkdwj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4of94n6sk3964mnr11hs6gkb2h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
